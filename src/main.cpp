52f64b2287a35a43d0766a536ea195154d2354cc
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-02-03 14:13:51 +0900

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void setup(void)
+{
+    M5.begin();
+}
+
+void loop(void)
+{
+    M5.update();
+}
---
+++ src/main.h
@@ -0,0 +1,7 @@
+#if defined(TARGET_M5STACK)
+#include <M5Stack.h>
+#elif defined(TARGET_M5STICK)
+#include <M5StickC.h>
+#elif defined(TARGET_M5ATOM)
+#include <M5Atom.h>
+#endif
