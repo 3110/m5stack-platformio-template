8d543e94e9e5ebc16090124fc8667d7201e18ea1
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-03-22 16:58:32 +0900

    リークの修正

--- src/common.cpp
+++ src/common.cpp
@@ -1,9 +1,14 @@
 #include "common.h"
 
 #ifdef ENABLE_SERIAL_MONITOR
+
+#ifndef ETH_ALEN
+#define ETH_ALEN 6
+#endif
+
 void printMacAddress(const uint8_t *macAddr, const bool newline)
 {
-    static char macStr[18];
+    static char macStr[ETH_ALEN * 3 + 1] = {0};
     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
              macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
     if (newline)
